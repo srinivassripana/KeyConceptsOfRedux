6a8ea647d43974efc1db0bc96d7b13520610986b
saisrinivas <saisrinivassripanasrkr@gmail.com>
2024-06-30 09:21:56 +0530

    this

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,46 @@
 import Restaurent from "./Restaurent";
-import resList from "../utils/mockData.js";
-import { useState } from "react";
+import { useState , useEffect } from "react";
+import Shimmer123456 from "../components/Shimmer123456";
 const Body = () => {
 
-  const [Listofres,setListofres] = useState(resList);
+  const [listOfRes ,setListOfRes] = useState([]);
+  
+  useEffect(
+    ()=>{
+       fetchdata();
+    }, []
+  );
+  
 
+  const fetchdata = async()=>{
+    const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+ 
+    const json = await data.json();
+    setListOfRes(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(listOfRes);
+};
+  if(!listOfRes?.length){
     return (
-       <div className="body">
-         <div className="filter">
-          <button 
-            className="filter-btn" 
-            onClick={() =>{
-              const filteredlist = Listofres.filter((res) => res.info.avgRating >= 4 );
-              setListofres(filteredlist);
-          }}>Button</button>
-         </div>
-         <div className="restaurentMenu">
-           {Listofres.map((restaurent, index) =>(
-            <Restaurent key={restaurent.info.id} resData ={restaurent}/>
-           ))}
-         </div>
-       </div>
-    );
+      <div className="body">
+        <div className="filter">
+         <button 
+           className="filter-btn" 
+           onClick={() =>{
+             const filteredlist = listOfRes.filter((res) => res.info.avgRating >= 4 );
+             setListOfRes(filteredlist);
+         }}>Button</button>
+        </div>
+        <div className="restaurentMenu">
+          {listOfRes.map((restaurent, index) =>(
+           <Restaurent key={restaurent.info.id} resData ={restaurent}/>
+          ))}
+        </div>
+      </div>
+   );
+  }
+
+  return <Shimmer123456/>;
+    
 };
 
 export default Body;
---
+++ src/components/Shimmer123456.js
@@ -0,0 +1,19 @@
+const Shimmer123456 = ()=>{
+    return 
+        <div className="shimmer-card">
+            <div className="shimmer-cards">jai</div>
+            <div className="shimmer-cards">jai</div>
+            <div className="shimmer-cards">jai</div>
+            <div className="shimmer-cards">jai</div>
+            <div className="shimmer-cards">jai</div>
+            <div className="shimmer-cards">jai</div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+        </div>
+    
+};
+
+export default Shimmer123456;
